85bb3f39bf51352e50ef5d5189b0d05b0df555a1
Aditya Singh <adityasinghboss1234@gmail.com>
2025-12-17 16:22:03 +0530

    feat: integrate chess.js for game logic and implement socket communication

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,3 +1,69 @@
-export const ChessBoard = () => {
-  return <div className="">Chess Board</div>;
+import type { Color, PieceSymbol, Square } from "chess.js";
+import { useState } from "react";
+
+const getSquareNotation = (row: number, col: number): Square => {
+  const file = String.fromCharCode("a".charCodeAt(0) + col);
+  const rank = 8 - row;
+  return `${file}${rank}` as Square;
+};
+
+export const ChessBoard = ({
+  socket,
+  board,
+}: {
+  socket: WebSocket;
+  board: ({
+    square: Square;
+    type: PieceSymbol;
+    color: Color;
+  } | null)[][];
+}) => {
+  const [from, setFrom] = useState<Square | null>(null);
+
+  return (
+    <div className="text-white-200">
+      {board.map((row, i) => {
+        return (
+          <div key={i} className="flex w-full">
+            {row.map((square, j) => {
+              const squareNotation = getSquareNotation(i, j);
+              return (
+                <div
+                  onClick={() => {
+                    if (!from) {
+                      if (square) setFrom(squareNotation);
+                    } else {
+                      const to = squareNotation;
+                      const move = from + to;
+
+                      try {
+                        socket.send(
+                          JSON.stringify({
+                            type: "move",
+                            move: move,
+                          })
+                        );
+                        console.log("Move sent:", move);
+                      } catch (e) {
+                        console.log("Invalid move:", move + e);
+                      }
+                      setFrom(null);
+                    }
+                  }}
+                  key={j}
+                  className={`w-16 h-16 ${
+                    (i + j) % 2 == 0 ? "bg-green-500" : "bg-slate-100"
+                  }`}
+                >
+                  <div className="w-full h-full flex justify-center items-center">
+                    {square ? square.type : ""}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,15 +1,72 @@
+import { useEffect, useState } from "react";
 import { ChessBoard } from "../components/ChessBoard";
+import { useSocket } from "../hooks/useSocket";
+import { Chess } from "chess.js";
 
 export const Game = () => {
+  const socket = useSocket();
+  const [chess, setChess] = useState(new Chess());
+  const [board, setBoard] = useState(chess.board());
+
+  useEffect(() => {
+    if (!socket) return;
+
+    // eslint-disable-next-line react-hooks/immutability
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+
+      switch (message.type) {
+        case "game_start":
+          chess.reset();
+          setBoard(chess.board());
+          console.log("Game started, color:", message.color);
+          break;
+        case "move":
+          chess.move(message.move);
+          setBoard(chess.board());
+          console.log("Move made:", message.move);
+          break;
+        case "game_over":
+          console.log("Game over:", message.outcome, message.method);
+          break;
+        case "waiting":
+          console.log("Waiting for opponent...");
+          break;
+        case "error":
+          console.log("Error:", message.message);
+          break;
+        default:
+          console.log("Unknown message:", message);
+          break;
+      }
+    };
+  }, [socket, chess]);
+
+  if (!socket)
+    return (
+      <div className="text-4xl font-bold">Connecting to game server ...</div>
+    );
+
   return (
     <div className="justify-center flex w-full">
       <div className="pt-8 max-w-5xl w-full">
         <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
-          <div className="md:col-span-4">
-            <ChessBoard />
+          <div className="md:col-span-4 w-full flex justify-center">
+            <ChessBoard socket={socket} board={board} />
           </div>
-          <div className="md:col-span-2">
-            <button>Play</button>
+          <div className="md:col-span-2 flex items-center justify-center">
+            <button
+              className="bg-green-400 text-white font-bold px-4 py-4 rounded-lg"
+              onClick={() => {
+                socket.send(
+                  JSON.stringify({
+                    type: "init_game",
+                  })
+                );
+              }}
+            >
+              Play
+            </button>
           </div>
         </div>
       </div>
